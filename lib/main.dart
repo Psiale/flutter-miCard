c588e8f5a14542be17c5fbe6af6bc0085959a70a
Alexis <psialedev@gmail.com>
2020-10-27 12:47:54 -0600

    learning basic of the container widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,26 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: Container(),
+        backgroundColor: Colors.purple,
+        body: SafeArea(
+          child: Container(
+            height: 100.0,
+            width: 100.0,
+            color: Colors.green,
+            margin: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
+            padding: EdgeInsets.all(20.0),
+            child: Text('sup'),
+          ),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
