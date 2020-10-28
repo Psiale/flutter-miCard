a8c64958cebe86ae47b12c66f4430a654a85777d
Alexis <psialedev@gmail.com>
2020-10-27 19:43:44 -0600

    finished the challenge project

--- lib/main.dart
+++ lib/main.dart
@@ -4,23 +4,53 @@ void main() {
   runApp(MyApp());
 }
 
+// Todas las propiedades de row aplican para column,
+// Si quieres expander un elemento puedes hacer que el main o el cross axis sean stretch
+
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.purple,
-        body: SafeArea(
-          child: Container(
-            height: 100.0,
-            width: 100.0,
-            color: Colors.green,
-            margin: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
-            padding: EdgeInsets.all(20.0),
-            child: Text('sup'),
+        appBar: AppBar(
+          backgroundColor: Colors.teal,
+        ),
+        body: Container(
+          color: Colors.teal,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              containerBuilder(Colors.red),
+              Container(
+                width: 100.0,
+                height: 200.0,
+                child: Column(
+                  children: [
+                    Container(
+                      width: 100.0,
+                      height: 100.0,
+                      color: Colors.yellow,
+                    ),
+                    Container(
+                        width: 100.0,
+                        height: 100.0,
+                        color: Color.fromRGBO(100, 200, 50, .7))
+                  ],
+                ),
+              ),
+              containerBuilder(Colors.blue)
+            ],
           ),
         ),
       ),
     );
   }
+}
+
+Widget containerBuilder(color) {
+  return Container(
+    width: 100.0,
+    color: color,
+  );
 }
